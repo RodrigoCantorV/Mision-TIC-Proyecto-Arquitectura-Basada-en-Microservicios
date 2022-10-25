b355a964ddcc0d68843fd7998aea5e19fe2f674f
Sergio Cantor Vasquez <scantor@unal.edu.co>
2022-10-25 12:20:15 -0500

    configuracion inicial del proyecto

--- index.py
+++ index.py
@@ -1,7 +1,42 @@
 from flask import Flask
+from flask import request
+from flask import jsonify
+from flask_cors import CORS
+from waitress import serve
+import json
 
 app = Flask(__name__)
+cors = CORS(app)
 
-@app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+###################################################################################
+# Importar controladores aqui
+
+###################################################################################
+@app.route("/",methods=['GET'])
+def test():
+    json = {}
+    json["message"]="Server running ..."
+    return jsonify(json)
+###################################################################################
+#PATHS DE PARTIDOS POLITICOS
+
+###################################################################################
+#PATHS DE CANDIDATOS
+
+###################################################################################
+#PATHS DE MESAS
+
+###################################################################################
+#PATHS DE RESULTADOS
+
+###################################################################################
+def loadFileConfig():
+    with open('config.json') as f:
+        data = json.load(f)
+    return data
+
+if __name__=='__main__':
+    dataConfig = loadFileConfig()
+    print("Server running : "+"http://"+dataConfig["url-backend"]+":" + str(dataConfig["port"]))
+    serve(app,host=dataConfig["url-backend"],port=dataConfig["port"])
+
