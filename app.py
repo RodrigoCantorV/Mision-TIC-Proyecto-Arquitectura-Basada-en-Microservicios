f53df07260abdd39227d4714a8a7321c65061f23
Sergio Cantor Vasquez <scantor@unal.edu.co>
2022-11-12 13:26:39 -0500

    Configuracion inicial del proyecto

--- app.py
+++ app.py
@@ -1,12 +1,26 @@
 from flask import Flask
+from flask import request
+from flask import jsonify
+from flask_cors import CORS
+from waitress import serve
+import requests
+import json
 
 app = Flask(__name__)
 
+cors = CORS(app)
 
 @app.route('/')
 def hello_world():  # put application's code here
     return 'Hello World!'
 
+def loadFileConfig():
+    with open('config.json') as f:
+        data = json.load(f)
+    return data
 
-if __name__ == '__main__':
-    app.run()
+if __name__=='__main__':
+    dataConfig = loadFileConfig()
+    print("Server running : "+"http://"+dataConfig["url-api-gateway"]+":" + str(dataConfig["puerto-gateway"]))
+    serve(app,host=dataConfig["url-api-gateway"],port=dataConfig["puerto-gateway"])
+
